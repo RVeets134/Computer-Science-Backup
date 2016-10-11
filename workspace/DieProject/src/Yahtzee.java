bd4e1f68cf4b29a7821d6672c03d951e83a4c9c3
RVeets134 <rakesh.veetekat@gmail.com>
2016-10-11 13:57:13 -0400

    Anotha One

---
+++ workspace/DieProject/src/Yahtzee.java
@@ -0,0 +1,17 @@
+
+public class Yahtzee {
+
+	public static void main(String[] args) {
+
+		Die[] dieArray = new Die[5];
+		
+		for(int n)
+		{
+			
+			System.out.println(dieArray[n].roll());
+			
+		}
+		
+	}
+
+}
