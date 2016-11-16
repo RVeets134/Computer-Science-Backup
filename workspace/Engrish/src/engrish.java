22c6c60f8c26bec365b2888fe5844f10d3846212
RVeets134 <rakesh.veetekat@gmail.com>
2016-11-16 13:56:40 -0500

    Screw wonjae

---
+++ workspace/Engrish/src/engrish.java
@@ -0,0 +1,16 @@
+public class engrish
+{
+
+	public static void main(String[] args) 
+	{
+		dictionary websters = new dictionary (100,10000);
+		System.out.println("Websters Dictionary");
+		System.out.println(websters);
+		System.out.println();
+		novel hots = new novel(150,55);
+		System.out.println("Gazlowe's World");
+		System.out.println(hots);
+
+	}
+
+}
